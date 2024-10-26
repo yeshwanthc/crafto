d2d4a1c8d45a887ece039a3328fa7268f2aacf2e
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-10-26 17:44:28 +0530

    lint issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Link from "next/link";
 import Header from "@/components/Header";
 import { useAuth } from "@/hooks/useAuth";
